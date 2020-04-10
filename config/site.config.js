622d7dafbba586126dd13ed6993bed71ec249578
JessicaNolette <63456212+JessicaNolette@users.noreply.github.com>
2020-04-10 17:52:46 -0400

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -9,13 +9,13 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'Static Site Boilerplate',
+  site_name: 'Dharma Dollars',
 
   // Your website's description, used for favicon meta tags
-  site_description: 'A modern boilerplate for static website development',
+  site_description: 'Saving money with positive intentions',
 
   // Your website's URL, used for sitemap
-  site_url: 'http://staticsiteboilerplate.com',
+  site_url: 'https://jessicanolette.github.io/Dharma-Dollars/',
 
   // Google Analytics tracking ID (leave blank to disable)
   googleAnalyticsUA: '',
